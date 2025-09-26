d306f360370ac9ebbf063a41f3be6c6086a97531
María Sol Crespi <167643787+solcrespi05@users.noreply.github.com>
2025-09-26 16:39:40 -0300

    modified:   factory/abstract_factory/ingredients.py

--- factory/abstract_factory/ingredients.py
+++ factory/abstract_factory/ingredients.py
@@ -52,31 +52,3 @@ class ChicagoPizzaIngredientFactory(PizzaIngredientFactory):
     def create_clam(self) -> Clams:   return Clams("Frozen Clams")
     def create_peperonni(self) -> Pepperoni: return Pepperoni("Sliced Pepperoni, Onion, Mushrooms")
     def create_veggie(self) -> Veggies: return Veggies("Spinach, Eggplant")
-
-class NYStyleVeggiePizzaIngredientFactory(PizzaIngredientFactory):
-    def create_dough(self) -> Dough:  return Dough("Thin Crust Dough")
-    def create_sauce(self) -> Sauce:  return Sauce("Marinara Sauce")
-    def create_cheese(self) -> Cheese:return Cheese("Reggiano Cheese")
-    def create_clam(self) -> Clams:   return Clams("Fresh Clams")
-    def create_veggie(self)-> Veggies: return Veggies("Grilled Veggies")
-
-class ChicagoStyleVeggiePizzaIngredientFactory(PizzaIngredientFactory):
-    def create_dough(self) -> Dough:  return Dough("Thick Crust Dough")
-    def create_sauce(self) -> Sauce:  return Sauce("Plum Tomato Sauce")
-    def create_cheese(self) -> Cheese:return Cheese("Mozzarella Cheese")
-    def create_clam(self) -> Clams:   return Clams("Frozen Clams")
-    def create_veggie(self)-> Veggies: return Veggies("Spinach, Eggplant")
-
-class NYStylePepperoniPizzaIngredientFactory(PizzaIngredientFactory):
-    def create_dough(self) -> Dough:  return Dough("Thin Crust Dough")
-    def create_sauce(self) -> Sauce:  return Sauce("Marinara Sauce")
-    def create_cheese(self) -> Cheese:return Cheese("Reggiano Cheese")
-    def create_clam(self) -> Clams:   return Clams("Fresh Clams")
-    def create_peperonni(self) -> Pepperoni: return Pepperoni("Sliced Peperonni")
-
-class ChicagoStylePepperoniPizzaIngredientFactory(PizzaIngredientFactory):
-    def create_dough(self) -> Dough:  return Dough("Thick Crust Dough")
-    def create_sauce(self) -> Sauce:  return Sauce("Plum Tomato Sauce")
-    def create_cheese(self) -> Cheese:return Cheese("Mozzarella Cheese")
-    def create_clam(self) -> Clams:   return Clams("Frozen Clams")
-    def create_peperonni(self) -> Pepperoni: return Pepperoni("Sliced Peperonni, Onion, Mushrooms")
